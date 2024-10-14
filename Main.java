80fb9d0992cef41a412d05276b9ac043a943798a
Veasna <p.v.veasna@gmail.com>
2024-10-14 15:00:12 +0700

    My version2 finsih

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
 public static void main(String[] args) {
     System.out.println("Main.java");
-    System.out.println("Version 1");
+    System.out.println("Version 2");
+    System.out.println("add code for version2");
 }
 }
