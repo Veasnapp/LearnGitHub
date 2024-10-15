fab4f185fe244e1808884af1d034fb278de5fd22
Veasna <p.v.veasna@gmail.com>
2024-10-15 14:37:28 +0700

    finish

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public static void main(String[] args) {
     System.out.println("Main.java");
     System.out.println("Version 2");
     System.out.println("add code for version2");
+    System.out.println("Fixing bug by me");
 }
 }
